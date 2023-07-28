a795a33952f3caa1affa17e090005bd4ebec9ee5
Omkar Kudale <omkar7890kudale@gmail.com>
2023-07-28 14:17:01 +0530

    file structure created

---
+++ src/components/Dashboard.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./dashboard.scss";
+import Section1 from "./sections/Section1";
+
+function Dashboard() {
+  return (
+    <>
+      <div className="dashboard">
+        <Section1 />
+      </div>
+    </>
+  );
+}
+
+export default Dashboard;
