f1811fa85736a9fc5a65116f6f4036a602460176
Omkar Kudale <omkar7890kudale@gmail.com>
2023-08-01 16:37:42 +0530

    section 8 completed

--- src/components/sections/Section8.js
+++ src/components/sections/Section8.js
@@ -1,23 +1,27 @@
-import React from 'react'
-import "./section8.scss"
-import ReviewCard from "../../cards/ReviewCard.js"
+import React from "react";
+import "./section8.scss";
+import ReviewCard from "../../cards/ReviewCard.js";
 
 const Section8 = () => {
   return (
-   <>
-   <div className="section8">
-    <div className="section8-container">
-        <div className="section8-heading-container">
-            <div className="section8-heading">Stories - From the next-gen <br /> developers of the world</div>
-        </div>
-        <div className="section8-card-wrapper">
+    <>
+      <div className="section8">
+        <div className="section8-container">
+          <div className="section8-heading-container">
+            <div className="section8-heading">
+              Stories - From the next-gen <br /> developers of the world
+            </div>
+          </div>
+          <div className="section8-card-wrapper">
             <ReviewCard></ReviewCard>
+            <ReviewCard></ReviewCard>
+            <ReviewCard></ReviewCard>
+            <ReviewCard></ReviewCard>
+          </div>
         </div>
-    </div>
-   </div>
-   
-   </>
-  )
-}
+      </div>
+    </>
+  );
+};
 
-export default Section8
+export default Section8;
