0499dd4342c63116f46951d763065e861763e118
Omkar Kudale <omkar7890kudale@gmail.com>
2023-08-03 20:24:43 +0530

    button added in section 3

--- src/components/sections/Section3.js
+++ src/components/sections/Section3.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react";
 import "./section3.scss";
 import Tilt from 'react-parallax-tilt';
+import { ArrowForward } from "@mui/icons-material";
 
 const Section3 = () => {
 
@@ -16,7 +17,10 @@ const Section3 = () => {
               You should have atleast 1+ years of proffessional experience in
               software developement
             </div>
-            <div className="big-card-btn"></div>
+            <div className="section3-button animation-button" type="button">
+              <span>Join Fellowship</span>
+              <ArrowForward id="arrow-id"></ArrowForward>
+            </div>
           </div>
         </div>
         </Tilt>
